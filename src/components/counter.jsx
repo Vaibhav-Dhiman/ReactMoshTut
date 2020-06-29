9621117f7cd8351d25671b4e581527c98daf5f73
vaibhav dhiman <princedhiman70@gmail.com>
2020-06-29 23:03:54 +0530

    state update by set

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,20 +3,29 @@ import React, { Fragment } from 'react';
 
 class counter extends React.Component {
     state = {
-        count: 0
+        count: 0,
+        tags: ['tag1', 'tag2', 'tag3']
     };
 
     render() { 
         return (
             <Fragment>
-            <span>{this.formatCount()}</span>
-            <button onClick="updateCount()">Increment</button>
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            <button className="btn btn-secondary btn-sm" onClick={this.updateCount.bind(this)}>Increment</button>
+              {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
             </Fragment>
         );
     }
 
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning": "primary"
+        return classes;
+    }
+
     updateCount() {
-        this.setState.count = this.state.count + 1;
+        this.setState({ count: this.state.count +1})
+        //console.log(this.state.count);
     }
 
     formatCount() {
