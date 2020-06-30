ca1b5c67ae87e8627ee6f17b4ee8474bfc2ece64
vaibhav dhiman <princedhiman70@gmail.com>
2020-06-30 15:13:34 +0530

    counter add need to check

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,42 +2,44 @@ import React, { Fragment } from 'react';
 
 
 class counter extends React.Component {
-    state = {
-        value: this.props.value,
-        tags: ['tag1', 'tag2', 'tag3']
-    };
+    // state = {
+    //     value: this.props.counter.value,
+    //     tags: ['tag1', 'tag2', 'tag3']
+    // };
 
 
-    // use arrow function as blow to not get error of state not defined or bind onclick with this keyword if not using arrow function. as
-    // {this.updateCount.bind(this)}
-    updateCount = () => {
-        this.setState({ value: this.state.value + 1 })
-        //console.log(this.state.count);
-    }
+    // // use arrow function as blow to not get error of state not defined or bind onclick with this keyword if not using arrow function. as
+    // // {this.updateCount.bind(this)}
+    // updateCount = () => {
+    //     this.setState({ value: this.state.value + 1 })
+    //     //console.log(this.state.count);
+    // }
 
-    render() { 
-        console.log('props', this.props);
+     render() { 
+     //   console.log('props', this.props);
 
         return (
             <Fragment>
             <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-            <button className="btn btn-secondary btn-sm" onClick={this.updateCount}>Increment</button>
-                 <li>{this.state.value}</li>
-              {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+            <button className="btn btn-secondary btn-sm m-2" onClick={this.props.onIncrement(this.props.counter)}>Increment</button>
+            <button className="btn btn-danger btn-sm m-2" onClick={() => this.props.onDelete(this.props.counter.id)}>Delete</button>
+
+                 {/* <li>{this.state.value}</li>
+              {this.state.tags.map(tag => <li key={tag}>{tag}</li>)} */}
             </Fragment>
         );
     }
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning": "primary"
+        classes += this.props.counter.value === 0 ? "warning": "primary"
         return classes;
     }
 
    
 
     formatCount() {
-        const { value } = this.state;
+        const { value } = this.props.counter;
         return value === 0 ? 'Zero' : value;
     }
 }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -5,16 +5,50 @@ class Counters extends React.Component {
     state = { 
         counters : [
             { id: 1, value: 1 },
-            { id: 2, value: 2 },
+            { id: 2, value: 0 },
             { id: 3, value: 3 },
         ]
      };
 
+     handleDelete = (counterId) => {
+        const counters = this.state.counters.filter(c => c.id !== counterId);
+        this.setState({ counters });
+
+    };
+
+    handleReset = () => {
+
+        const counters =this.state.counters.map(c => {
+            c.value = 0;
+            return c;
+        });
+        this.setState({ counters })
+    };
+
+    handleIncrement = counter => {
+        console.log(counter);
+        // const counters = [...this.state.counters];
+        // const index = counters.indexOf(counter);
+        // counters[index] = {...counter};
+        // counters[index].value++;
+        // this.setState({ counters });
+    };
+
     render() { 
         return ( 
             <div>
+                <button onClick={this.handleReset}
+                className="btn btn-primary btn-sm m-2">
+                    Reset
+                    </button>
               {this.state.counters.map(counter =>
-                 <Counter key={counter.id} value={counter.value} selected = {true}/>)}
+                 <Counter 
+                 key={counter.id} 
+                 onDelete={()=> this.handleDelete(counter.id)} 
+                 onIncrement = {this.handleIncrement}
+                 //onDelete={this.handleDelete} 
+                 counter = {counter}
+                 />)}
             </div>  
          );
     }
